fafd341d712a5742b3057b37f968b89e6c0addc3
anindyapramudita <anindyapramudita2111@gmail.com>
2022-08-21 11:06:40 +0700

    PHRG2-9: Fixed route on App.js, Created a global navbar, update the toast and snackbar

--- projects/client/src/Pages/Auth/Partials/TokenExpiredPage.jsx
+++ projects/client/src/Pages/Auth/Partials/TokenExpiredPage.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Container, Grid, Toolbar, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const TokenExpiredPage = () => {
