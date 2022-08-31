e6726abf1f235916257b03e292492515a90e796b
Abdi-01 <abdialghi@gmail.com>
2022-08-31 17:56:28 +0700

    development-abdi : modify import env

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -1,7 +1,7 @@
-require("dotenv/config");
+const { join } = require("path");
+require('dotenv').config({ path: join(__dirname, '../.env') });
 const express = require("express");
 const cors = require("cors");
-const { join } = require("path");
 const bearerToken = require('express-bearer-token')
 
 const PORT = process.env.PORT || 8000;
