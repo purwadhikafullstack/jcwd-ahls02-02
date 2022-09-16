f166240b76c280f258465e13d9df5d083b7ff4b8
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2022-09-16 17:59:53 +0700

    Update helper.js

--- projects/client/src/helper.js
+++ projects/client/src/helper.js
@@ -1,3 +1,3 @@
 export const FE_URL = "https://jcwd-ahls02-02.purwadhikabootcamp.com"
 export const API_URL = "https://jcwd-ahls02-02.purwadhikabootcamp.com/api"
-export const API_IMAGE_URL = "https://jcwd-ahls02-02.purwadhikabootcamp.com/"
+export const API_IMAGE_URL = "https://jcwd-ahls02-02.purwadhikabootcamp.com"
