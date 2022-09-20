c8997f65b6420c4365e3af2567f2aa91bfca0ae5
c-kevin-j <kevin_julius@srwasia.com>
2022-09-20 21:44:15 +0700

    update product card component

--- projects/client/src/Components/ProductCard2.jsx
+++ projects/client/src/Components/ProductCard2.jsx
@@ -1,11 +1,10 @@
 import { Box, Card, CardContent, CardMedia, Tooltip } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import Button from "./atoms/Button";
 import Text from "./atoms/Text";
 
 const ProductCard = (props) => {
-  const { id, name, price, image, quantity, handleDetail } = props;
-  const navigate = useNavigate();
+  const { id, name, price, image, quantity, needs_receipt, handleDetail } =
+    props;
 
   return (
     <Tooltip title={name} followCursor>
@@ -26,6 +25,7 @@ const ProductCard = (props) => {
             alignItems: "center",
             justifyContent: "center",
             p: 1,
+            position: "relative",
           }}
         >
           <CardMedia
@@ -34,6 +34,34 @@ const ProductCard = (props) => {
             image={image}
             sx={{ maxWidth: "100%", maxHeight: "100%" }}
           />
+          {needs_receipt === "true" && (
+            <Box
+              sx={{
+                backgroundColor: "#F7C749",
+                // borderRadius: "5px",
+                p: "5px",
+                position: "absolute",
+                top: "10px",
+                left: "-2px",
+              }}
+            >
+              <Text
+                fontSize="caption"
+                fontWeight="bold"
+                color="white"
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  textAlign: "left",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                }}
+              >
+                Needs Prescription
+              </Text>
+            </Box>
+          )}
         </Box>
         <Box sx={{ width: { xs: "50%", sm: "100%" }, my: "auto" }}>
           <CardContent>
@@ -53,24 +81,28 @@ const ProductCard = (props) => {
             <Text fontSize="body2" color="grey.600" sx={{ textAlign: "left" }}>
               IDR {price.toLocaleString()}
             </Text>
-            {quantity > 0 ?
-              <Text fontSize="subtitle2" color="grey.600" sx={{ textAlign: "left" }}>
+            {quantity > 0 ? (
+              <Text
+                fontSize="subtitle2"
+                color="grey.600"
+                sx={{ textAlign: "left" }}
+              >
                 Stock: {quantity}
               </Text>
-              :
-              <Text fontSize="subtitle2" color="error" sx={{ textAlign: "left", fontStyle: "italic" }}>
+            ) : (
+              <Text
+                fontSize="subtitle2"
+                color="error"
+                sx={{ textAlign: "left", fontStyle: "italic" }}
+              >
                 Out of stock
               </Text>
-            }
-            {/* <Text fontSize="subtitle2" color="grey.600" sx={{ textAlign: "left" }}>
-              Stock: {quantity}
-            </Text> */}
+            )}
 
             <Button
               variant="contained"
               sx={{ width: "100%", mt: 1 }}
               onClick={() => handleDetail(id)}
-            // onClick={() => navigate(`/product/${id}`)}
             >
               Detail
             </Button>
--- projects/client/src/Pages/Products/ProductPage.jsx
+++ projects/client/src/Pages/Products/ProductPage.jsx
@@ -9,227 +9,226 @@ import ProductCards from "./partials/ProductPage/ProductCards";
 import { useLocation } from "react-router-dom";
 
 function useQuery() {
-    const { search } = useLocation();
+  const { search } = useLocation();
 
-    return useMemo(() => new URLSearchParams(search), [search]);
+  return useMemo(() => new URLSearchParams(search), [search]);
 }
 
 const ProductPage = () => {
+  let query = useQuery();
 
-    let query = useQuery();
+  const [name, setName] = useState();
+  const [idCategory, setIdCategory] = useState(query.get("id_category"));
+  const [needsReceipt, setNeedsReceipt] = useState();
+  const [minPrice, setMinPrice] = useState();
+  const [maxPrice, setMaxPrice] = useState();
+  const [sort, setSort] = useState("");
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(32);
 
-    const [name, setName] = useState();
-    const [idCategory, setIdCategory] = useState(query.get("id_category"));
-    const [needsReceipt, setNeedsReceipt] = useState();
-    const [minPrice, setMinPrice] = useState();
-    const [maxPrice, setMaxPrice] = useState();
-    const [sort, setSort] = useState("");
-    const [page, setPage] = useState(1);
-    const [limit, setLimit] = useState(32);
+  const [productData, setProductData] = useState();
+  const [category, setCategory] = useState();
+  const [totalPage, setTotalPage] = useState();
 
-    const [productData, setProductData] = useState();
-    const [category, setCategory] = useState();
-    const [totalPage, setTotalPage] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch();
+  const { search } = useLocation();
 
-    const dispatch = useDispatch()
-    const { search } = useLocation()
+  useEffect(() => {
+    getData();
+    getCategory();
+    // console.log('query.get("id_category")', query.get("id_category"))
+    // setIdCategory(query.get("id_category"))
+  }, []);
 
-    useEffect(() => {
-        getData();
-        getCategory();
-        // console.log('query.get("id_category")', query.get("id_category"))
-        // setIdCategory(query.get("id_category"))
-    }, []);
+  const getCategory = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/products/categories`);
+      if (res) {
+        setCategory(res.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    const getCategory = async () => {
-        try {
-            const res = await axios.get(`${API_URL}/products/categories`);
-            if (res) {
-                setCategory(res.data);
-            }
-        } catch (error) {
-            console.log(error);
+  const getData = async (
+    filterName = name,
+    filterCategory = idCategory,
+    filterNeedsReceipt = needsReceipt,
+    filterMinPrice = minPrice,
+    filterMaxPrice = maxPrice,
+    filterSort = sort,
+    filterPage = page,
+    filterLimit = limit
+  ) => {
+    try {
+      let query = "";
+
+      if (filterName) {
+        if (query) {
+          query += `&name=${filterName}`;
+        } else {
+          query += `?name=${filterName}`;
         }
-    };
+      }
 
-    const getData = async (
-        filterName = name,
-        filterCategory = idCategory,
-        filterNeedsReceipt = needsReceipt,
-        filterMinPrice = minPrice,
-        filterMaxPrice = maxPrice,
-        filterSort = sort,
-        filterPage = page,
-        filterLimit = limit
-    ) => {
-        try {
-            let query = "";
-
-            if (filterName) {
-                if (query) {
-                    query += `&name=${filterName}`;
-                } else {
-                    query += `?name=${filterName}`;
-                }
-            }
-
-            if (filterCategory) {
-                if (query) {
-                    query += `&id_category=${filterCategory}`;
-                } else {
-                    query += `?id_category=${filterCategory}`;
-                }
-            }
-
-            if (filterNeedsReceipt) {
-                if (query) {
-                    query += `&needs_receipt=${filterNeedsReceipt}`;
-                } else {
-                    query += `?needs_receipt=${filterNeedsReceipt}`;
-                }
-            }
-
-            if (filterMinPrice) {
-                if (query) {
-                    query += `&min_price=${filterMinPrice}`;
-                } else {
-                    query += `?min_price=${filterMinPrice}`;
-                }
-            }
-
-            if (filterMaxPrice) {
-                if (query) {
-                    query += `&max_price=${filterMaxPrice}`;
-                } else {
-                    query += `?max_price=${filterMaxPrice}`;
-                }
-            }
-
-            if (filterPage) {
-                if (query) {
-                    query += `&page=${filterPage}`;
-                } else {
-                    query += `?page=${filterPage}`;
-                }
-            }
-
-            if (filterLimit) {
-                if (query) {
-                    query += `&limit=${filterLimit}`;
-                } else {
-                    query += `?limit=${filterLimit}`;
-                }
-            }
-
-            if (filterSort) {
-                if (query) {
-                    query += `&${filterSort}`;
-                } else {
-                    query += `?${filterSort}`;
-                }
-            }
-
-            const response = await axios.get(`${API_URL}/products${query}`);
-
-            if (response) {
-                let data = [];
-                response.data.product.forEach((value) => {
-                    if (value.default_unit === "true") {
-                        data.push({
-                            id: value.id,
-                            name: value.name,
-                            category: value.category_name,
-                            image: value.image,
-                            price: value.selling_price,
-                            needs_receipt: value.needs_receipt,
-                            description: value.description,
-                            quantity: value.quantity,
-                        });
-                    }
-                });
-                dispatch(getProductsDataAction(data))
-                setProductData(data);
-                setTotalPage(response.data.totalPage);
-                setIsLoading(false);
-            }
-        } catch (error) {
-            console.log(error);
+      if (filterCategory) {
+        if (query) {
+          query += `&id_category=${filterCategory}`;
+        } else {
+          query += `?id_category=${filterCategory}`;
         }
-    };
+      }
 
-    const getDataFilter = (
-        newName,
-        newIdCategory,
-        newMinPrice,
-        newMaxPrice,
-        newSort
-    ) => {
-        setName(newName);
-        setIdCategory(newIdCategory);
-        setMinPrice(newMinPrice);
-        setMaxPrice(newMaxPrice);
-        setSort(newSort);
-        setPage(1);
+      if (filterNeedsReceipt) {
+        if (query) {
+          query += `&needs_receipt=${filterNeedsReceipt}`;
+        } else {
+          query += `?needs_receipt=${filterNeedsReceipt}`;
+        }
+      }
 
-        getData(
-            newName,
-            newIdCategory,
-            needsReceipt,
-            newMinPrice,
-            newMaxPrice,
-            newSort,
-            1,
-            limit
-        );
-    };
+      if (filterMinPrice) {
+        if (query) {
+          query += `&min_price=${filterMinPrice}`;
+        } else {
+          query += `?min_price=${filterMinPrice}`;
+        }
+      }
 
-    const handlePage = (newPage) => {
-        setPage(newPage);
-        getData(
-            name,
-            idCategory,
-            needsReceipt,
-            minPrice,
-            maxPrice,
-            sort,
-            newPage,
-            limit
-        );
-    };
+      if (filterMaxPrice) {
+        if (query) {
+          query += `&max_price=${filterMaxPrice}`;
+        } else {
+          query += `?max_price=${filterMaxPrice}`;
+        }
+      }
 
-    return (
-        <div style={{ paddingBottom: '5rem' }}>
-            {!isLoading && (
-                <Container>
-                    <Grid container>
-                        {/* Filter */}
-                        <Grid xs={12} md={3} sx={{ p: 1 }}>
-                            <FilterProducts
-                                getDataFilter={getDataFilter}
-                                name={name}
-                                idCategory={idCategory}
-                                minPrice={minPrice}
-                                maxPrice={maxPrice}
-                                sort={sort}
-                                category={category}
-                            />
-                        </Grid>
-                        {/* Product Cards */}
-                        <Grid xs={12} md={9} sx={{ pt: 4, pl: 0 }}>
-                            <ProductCards
-                                productData={productData}
-                                totalPage={totalPage}
-                                changePage={handlePage}
-                                page={page}
-                            />
-                        </Grid>
-                    </Grid>
-                </Container>
-            )}
-        </div>
+      if (filterPage) {
+        if (query) {
+          query += `&page=${filterPage}`;
+        } else {
+          query += `?page=${filterPage}`;
+        }
+      }
+
+      if (filterLimit) {
+        if (query) {
+          query += `&limit=${filterLimit}`;
+        } else {
+          query += `?limit=${filterLimit}`;
+        }
+      }
+
+      if (filterSort) {
+        if (query) {
+          query += `&${filterSort}`;
+        } else {
+          query += `?${filterSort}`;
+        }
+      }
+
+      const response = await axios.get(`${API_URL}/products${query}`);
+
+      if (response) {
+        let data = [];
+        response.data.product.forEach((value) => {
+          if (value.default_unit === "true") {
+            data.push({
+              id: value.id,
+              name: value.name,
+              category: value.category_name,
+              image: value.image,
+              price: value.selling_price,
+              needs_receipt: value.needs_receipt,
+              description: value.description,
+              quantity: value.quantity,
+            });
+          }
+        });
+        dispatch(getProductsDataAction(data));
+        setProductData(data);
+        setTotalPage(response.data.totalPage);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getDataFilter = (
+    newName,
+    newIdCategory,
+    newMinPrice,
+    newMaxPrice,
+    newSort
+  ) => {
+    setName(newName);
+    setIdCategory(newIdCategory);
+    setMinPrice(newMinPrice);
+    setMaxPrice(newMaxPrice);
+    setSort(newSort);
+    setPage(1);
+
+    getData(
+      newName,
+      newIdCategory,
+      needsReceipt,
+      newMinPrice,
+      newMaxPrice,
+      newSort,
+      1,
+      limit
     );
+  };
+
+  const handlePage = (newPage) => {
+    setPage(newPage);
+    getData(
+      name,
+      idCategory,
+      needsReceipt,
+      minPrice,
+      maxPrice,
+      sort,
+      newPage,
+      limit
+    );
+  };
+
+  return (
+    <div style={{ paddingBottom: "5rem" }}>
+      {!isLoading && (
+        <Container>
+          <Grid container>
+            {/* Filter */}
+            <Grid xs={12} md={3} sx={{ p: 1 }}>
+              <FilterProducts
+                getDataFilter={getDataFilter}
+                name={name}
+                idCategory={idCategory}
+                minPrice={minPrice}
+                maxPrice={maxPrice}
+                sort={sort}
+                category={category}
+              />
+            </Grid>
+            {/* Product Cards */}
+            <Grid xs={12} md={9} sx={{ pt: 4, pl: 0 }}>
+              <ProductCards
+                productData={productData}
+                totalPage={totalPage}
+                changePage={handlePage}
+                page={page}
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      )}
+    </div>
+  );
 };
 
 export default ProductPage;
